e7b1b559cbf3e9d67dacc47f248f07dd161a51ca
XiaoYun <15543495640m@sina.cn>
2024-08-04 19:05:31 +0800

    note

---
+++ public/anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2024/01/01/hexo/npm图床使用/","2024/01/12/svn/svn入门/","2024/01/14/svn/TortoiseSVN基本使用/","2024/01/16/svn/svn仓库目录详解/","2024/01/17/algorithm/数位DP/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
