059ef4ed4fb64870e84b6b7f2fb80ce1d7694d96
lqyspace <15543495640m@sina.cn>
2025-07-06 16:02:40 +0800

    new blog

--- public/anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/01/17/algorithm/数位DP/","2024/01/14/svn/TortoiseSVN基本使用/","2024/01/16/svn/svn仓库目录详解/","2024/01/12/svn/svn入门/","2024/01/01/hexo/npm图床使用/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
